88d32812e8f977e7913e19a0e425fbe40bfbdb11
rachneum <rachel-1604@hotmail.com>
2024-05-03 16:19:19 +0200

    iris

--- main.c
+++ main.c
@@ -6,9 +6,27 @@
 /*   By: rachou <rachou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 11:51:01 by rachou            #+#    #+#             */
-/*   Updated: 2024/05/01 14:18:58 by rachou           ###   ########.fr       */
+/*   Updated: 2024/05/03 16:14:59 by rachou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;//mlx window.
+	t_data	*img;//image.
+
+	mlx = mlx_init();
+	if (!mlx)
+		return (1);
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");//1920 = largeur, 1080 = hauteur.
+	img = NULL;
+	img->img = mlx_new_image(mlx, 1920, 1080);
+	img->addr = mlx_get_data_addr(img->img, &img->bits_per_pixel, &img->line_length, &img->endian);
+	my_mlx_pixel_put(img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img->img, 0, 0);
+	mlx_loop(mlx);
+	return (0);
+}
--- so_long_utils.c
+++ so_long_utils.c
@@ -1,33 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.h                                          :+:      :+:    :+:   */
+/*   so_long_utils.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rachou <rachou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/26 15:19:59 by rachou            #+#    #+#             */
-/*   Updated: 2024/05/03 16:15:18 by rachou           ###   ########.fr       */
+/*   Created: 2024/05/03 15:20:32 by rachou            #+#    #+#             */
+/*   Updated: 2024/05/03 15:31:20 by rachou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG_H
-# define SO_LONG_H
+#include "so_long.h"
 
-#include "mlx/mlx.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-
-typedef struct s_data
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
-    void	*img;
-    char	*addr;
-    int		bits_per_pixel;
-    int		line_length;
-    int		endian;
-}	t_data;
+	char	*dst;
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
-
-#endif
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
