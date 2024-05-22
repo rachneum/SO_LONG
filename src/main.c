55594c43eca4153d9c0c40657cdb667d3b895c32
rachneum <rachel-1604@hotmail.com>
2024-05-22 16:57:01 +0200

    rose

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rachou <rachou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 11:51:01 by rachou            #+#    #+#             */
-/*   Updated: 2024/05/16 17:34:32 by rachou           ###   ########.fr       */
+/*   Updated: 2024/05/22 16:52:46 by rachou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,20 @@
 int	main(void)
 {
 	void	*mlx;
-	void	*mlx_win;//mlx window.
-	t_data	*img;//image.
+	void	*mlx_win;
+	t_data	*img;
 
 	mlx = mlx_init();
 	if (!mlx)
 		return (1);
-	mlx_win = mlx_new_window(mlx, 600, 600, "Hello world!");//1920 = largeur, 1080 = hauteur.
+	mlx_win = mlx_new_window(mlx, 600, 600, "Hello world!");
 	img = malloc(sizeof(t_data));
 	if (!img)
 		return (1);
 	img->img = mlx_new_image(mlx, 600, 600);
 	img->addr = mlx_get_data_addr(img->img, &img->bits_per_pixel, &img->line_length, &img->endian);
-	my_mlx_pixel_put(img, 5, 5, 0x00FF0000);
 	mlx_put_image_to_window(mlx, mlx_win, img->img, 0, 0);
 	mlx_loop(mlx);
 	free(img);
 	return (0);
-}
-
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
-{
-	char	*dst;
-
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
 }
