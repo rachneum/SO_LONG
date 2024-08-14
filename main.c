3ca7d5d272cbc4846a559b9919ef04bd2470a83f
Rachel Neumann <raneuman@fu-r10-p3.s19.be>
2024-08-14 14:10:17 +0200

    sunflower

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rachou <rachou@student.42.fr>              +#+  +:+       +#+        */
+/*   By: raneuman <raneuman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 13:52:30 by rachou            #+#    #+#             */
-/*   Updated: 2024/08/09 14:49:52 by rachou           ###   ########.fr       */
+/*   Updated: 2024/08/14 14:10:00 by raneuman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ int	main(int argc, char **argv)
 	t_data	game;
 	int		len;
 
-	len = ft_strlen(argv[1]);
 	if (argc != 2)
 		ft_error("ERROR\nThere is not the right amount of arguments!\n");
+	len = ft_strlen(argv[1]);
 	if ((ft_strncmp(argv[1] + len - 4, ".ber", 4)) != 0)
 		ft_error("ERROR\nMap format is invalid!\n");
 	parse_map(&game, argv[1]);
